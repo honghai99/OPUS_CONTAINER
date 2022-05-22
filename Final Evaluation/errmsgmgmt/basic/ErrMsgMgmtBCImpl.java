40f6efbdca7334ea5bf250ccbc1a5950b3f31c41
honghai99 <105183014+honghai99@users.noreply.github.com>
2022-05-22 19:23:05 +0700

    Update ErrMsgMgmtBCImpl.java

--- Final Evaluation/errmsgmgmt/basic/ErrMsgMgmtBCImpl.java
+++ Final Evaluation/errmsgmgmt/basic/ErrMsgMgmtBCImpl.java
@@ -25,7 +25,7 @@ import com.clt.apps.opus.esm.clv.newgen.errmsgmgmt.vo.ErrMsgVO;
 
 /**
  * ALPS-newgen Business Logic Command Interface<br>
- * - ALPS-newgen에 대한 비지니스 로직에 대한 인터페이스<br>
+ * 
  *
  * @author Hai To
  * @since J2EE 1.6
@@ -36,14 +36,12 @@ public class ErrMsgMgmtBCImpl extends BasicCommandSupport implements ErrMsgMgmtB
 	private transient ErrMsgMgmtDBDAO dbDao = null;
 
 	/**
-	 * ErrMsgMgmtBCImpl 객체 생성<br>
-	 * ErrMsgMgmtDBDAO를 생성한다.<br>
 	 */
 	public ErrMsgMgmtBCImpl() {
 		dbDao = new ErrMsgMgmtDBDAO();
 	}
 	/**
-	 * [비즈니스대상]을 [행위] 합니다.<br>
+	 * 
 	 * 
 	 * @param ErrMsgVO errMsgVO
 	 * @return List<ErrMsgVO>
@@ -56,12 +54,11 @@ public class ErrMsgMgmtBCImpl extends BasicCommandSupport implements ErrMsgMgmtB
 			throw new EventException(new ErrorHandler(ex).getMessage(),ex);
 		} catch (Exception ex) {
 			throw new EventException(new ErrorHandler(ex).getMessage(),ex);
-		}
-		
+		}	
 	}
 	
 	/**
-	 * [비즈니스대상]을 [행위] 합니다.<br>
+	 * 
 	 * 
 	 * @param ErrMsgVO[] errMsgVO
 	 * @param account SignOnUserAccount
@@ -72,14 +69,23 @@ public class ErrMsgMgmtBCImpl extends BasicCommandSupport implements ErrMsgMgmtB
 			List<ErrMsgVO> insertVoList = new ArrayList<ErrMsgVO>();
 			List<ErrMsgVO> updateVoList = new ArrayList<ErrMsgVO>();
 			List<ErrMsgVO> deleteVoList = new ArrayList<ErrMsgVO>();
+					
 			for ( int i=0; i<errMsgVO .length; i++ ) {
 				if ( errMsgVO[i].getIbflag().equals("I")){
-					errMsgVO[i].setCreUsrId(account.getUsr_id());
-					errMsgVO[i].setUpdUsrId(account.getUsr_id());
-					insertVoList.add(errMsgVO[i]);
-				} else if ( errMsgVO[i].getIbflag().equals("U")){
-					errMsgVO[i].setUpdUsrId(account.getUsr_id());
-					updateVoList.add(errMsgVO[i]);
+					if(checkDuplicate(errMsgVO[i]).size()>0) {
+						throw new EventException(new ErrorHandler("ERR99999").getMessage());
+					} else {
+						errMsgVO[i].setCreUsrId(account.getUsr_id());
+						errMsgVO[i].setUpdUsrId(account.getUsr_id());
+						insertVoList.add(errMsgVO[i]);
+					}
+				} else if (errMsgVO[i].getIbflag().equals("U")){
+					if(checkDuplicate(errMsgVO[i]).size()>0) {
+						throw new EventException(new ErrorHandler("ERR99999").getMessage());
+					} else {
+						errMsgVO[i].setUpdUsrId(account.getUsr_id());
+						updateVoList.add(errMsgVO[i]);
+					}
 				} else if ( errMsgVO[i].getIbflag().equals("D")){
 					deleteVoList.add(errMsgVO[i]);
 				}
@@ -103,6 +109,13 @@ public class ErrMsgMgmtBCImpl extends BasicCommandSupport implements ErrMsgMgmtB
 		}
 	}
 	
+	
+	/**
+	 * Search carrier for combo box
+	 * @param errMsgVO
+	 * @return
+	 * @throws EventException
+	 */
 	@Override
 	public List<ErrMsgVO> searchCarrier(ErrMsgVO errMsgVO) throws EventException {
 		try {
@@ -113,10 +126,30 @@ public class ErrMsgMgmtBCImpl extends BasicCommandSupport implements ErrMsgMgmtB
 			throw new EventException(new ErrorHandler(ex).getMessage(),ex);
 		}	
 	}
+	
+	/**
+	 * Search lane for combobox
+	 * @param errMsgVO
+	 * @return
+	 * @throws EventException
+	 */
 	@Override
 	public List<ErrMsgVO> searchLane(ErrMsgVO errMsgVO) throws EventException {		
 		try {
 			return dbDao.searchLane(errMsgVO);
+		} catch (DAOException e) {
+			throw new EventException(new ErrorHandler(e).getMessage(),e);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new EventException(new ErrorHandler(e).getMessage(),e);
+
+		}
+	}
+	
+	@Override
+	public List<ErrMsgVO> checkDuplicate(ErrMsgVO errMsgVO) throws EventException {
+		try {
+			return dbDao.checkDuplicate(errMsgVO);
 		} catch (DAOException e) {
 			throw new EventException(new ErrorHandler(e).getMessage(),e);
 		} catch (Exception e) {
